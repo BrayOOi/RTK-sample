604698b533a1278b44f3aaf77269e76a149d0f07
Brayden Ooi <brayden.ooi@hotmail.com>
2022-01-18 17:10:45 +0800

    organize hooks folder

---
+++ src/app/hooks/index.ts
@@ -0,0 +1,6 @@
+// Add hooks that are used frequently together here
+// Can always import directly from the respective dedicated files if the hook is to be consumed independently
+import useAppDispatch from './useAppDispatch';
+import useAppSelector from './useAppSelector';
+
+export { useAppDispatch, useAppSelector };
